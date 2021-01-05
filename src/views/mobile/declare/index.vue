119435610069eada54c75257ef8562497f3a6144
hf511 <577690236@qq.com>
2021-01-05 15:19:33 +0800

    修改bug

--- src/views/mobile/declare/index.vue
+++ src/views/mobile/declare/index.vue
@@ -958,7 +958,7 @@
         height: 44px;
         float: right;
         font-size: 32px;
-        font-family: PingFang SC;
+        font-family: Microsoft YaHei;
         font-weight: 400;
         line-height: 44px;
         cursor: pointer;
